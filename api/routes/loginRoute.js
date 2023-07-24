50de64e344bbcacf93b297a1e77b0df5cb3b49c7
unknown <mateusvpdl@outlook.com>
2023-07-24 14:02:30 -0400

    first commit

---
+++ api/routes/loginRoute.js
@@ -0,0 +1,12 @@
+const { Router } = require('express');
+const LoginController = require('../controllers/LoginController');
+
+const router = Router()
+
+router.get('/login', LoginController.validaLogin);
+//router.get('/login', loginController.getAll);
+router.post('/login', LoginController.criaLogin);
+router.put('/login/:id', LoginController.atualizaLogin);
+router.delete('/login/:id', LoginController.apagaLogin);
+
+module.exports = router
