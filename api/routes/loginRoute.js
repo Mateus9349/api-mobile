fcb3117434d1676abab194944aa7bf3a9af997e3
unknown <mateusvpdl@outlook.com>
2023-07-25 09:26:36 -0400

    modificando as rotas

--- api/routes/loginRoute.js
+++ api/routes/loginRoute.js
@@ -3,9 +3,10 @@ const LoginController = require('../controllers/LoginController');
 
 const router = Router()
 
-router.get('/login', LoginController.validaLogin);
+//router.get('/login', LoginController.validaLogin);
 //router.get('/login', loginController.getAll);
-router.post('/login', LoginController.criaLogin);
+router.post('/loginValidacao', LoginController.validaLogin);
+router.post('/loginCadastro', LoginController.criaLogin);
 router.put('/login/:id', LoginController.atualizaLogin);
 router.delete('/login/:id', LoginController.apagaLogin);
 
