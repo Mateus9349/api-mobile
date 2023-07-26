f5e5645852a476b88631d43296e81e9ec5231b7d
unknown <mateusvpdl@outlook.com>
2023-07-26 13:38:44 -0400

    corrigin bug da cheve autoincremente

--- api/migrations/20230724154248-create-usuario.js
+++ api/migrations/20230724154248-create-usuario.js
@@ -7,7 +7,7 @@ module.exports = {
         allowNull: false,
         autoIncrement: true,
         primaryKey: true,
-        type: Sequelize.UUIDV4,
+        type: Sequelize.UUID,
         defaultValue: Sequelize.UUIDV4
       },
       UF: {
