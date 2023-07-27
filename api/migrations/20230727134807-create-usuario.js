f31919a7da7d3ed6cec6365e016caf7a773aaa45
unknown <mateusvpdl@outlook.com>
2023-07-27 09:49:09 -0400

    corrigin bug da cheve autoincremente

--- api/migrations/20230727134807-create-usuario.js
+++ api/migrations/20230727134807-create-usuario.js
@@ -1,6 +1,5 @@
 'use strict';
-const { DataTypes } = require('sequelize');
-
+/** @type {import('sequelize-cli').Migration} */
 module.exports = {
   async up(queryInterface, Sequelize) {
     await queryInterface.createTable('Usuarios', {
